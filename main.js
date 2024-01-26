32bd107cf767c03f6967568d483c12916ac3c6df
Vendel098 <tothvendelcsaba@gmail.com>
2024-01-26 12:13:09 +0100

    ez így még nem jó

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-$('.tab1').click(function(){
-  $('.pizza1').css('display','block');
-  $('.pasta1').css('display', 'none');
-  $('.starter1').css('display', 'none');
-});
-$('.tab2').click(function(){
-  $('.pasta1').css('display', 'block')
-  $('.starter1').css('display', 'none');
-  $('.pizza1').css('display', 'none');
-  
-}); 
-$('.tab3').click(function(){
-  $('.starter1').css('display', 'block')
-  $('.pizza1').css('display', 'none');
-  $('.pasta1').css('display', 'none');
-});
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
